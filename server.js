5f69c68953df919f031f47dbd9b6ea9850bb3356
Sanskargh7 <93363422+Sanskargh7@users.noreply.github.com>
2023-08-10 11:56:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 // app.get("/", (req, res) => {
 //   res.send("<h1>Welcome to ecommerce app</h1>");
 // });
-app.use("/*", (req, res) => {
+app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
